5ba8e1bed6b0c5185dc12d371c36c111f096ff3d
qqmbers_eye <aa.petrovskaya@gmail.com>
2013-02-10 20:00:46 +0400

    общий каркас приложения, сортировка матрицы

--- fca/fca-common/src/main/java/ru/vsu/fca/core/FcaObjectSet.java
+++ fca/fca-common/src/main/java/ru/vsu/fca/core/FcaObjectSet.java
@@ -34,17 +34,44 @@ public class FcaObjectSet {
 
 	public void sortMatrix(){
 		
+		
+		
 	}
 	
-	public String getObjectById(int id){
-		return "";
+	public FcaObject getObjectById(int id){
+		FcaObject obj;
+		obj = this.objects.get(id);
+		return obj;
 	}
 	
-	public String getAttributeById(int id){
-		return "";
+	public FcaAttribute getAttributeById(int id){
+		FcaAttribute attr;
+		attr = this.attributes.get(id);
+		return attr;
 	}
 	
 	public int[] addElement(int[] attrVector){
 		return null;
 	}
+	
+	//алгоритм на языке java 
+	  public static void qSort(int[][] A, int low, int high) {
+	      int i = low;                
+	      int j = high;
+	      int x = A[(low+high)/2][1];  // x - опорный элемент посредине между low и high
+	      do {
+	          while(A[i][1] < x) ++i;  // поиск элемента для переноса в старшую часть
+	          while(A[j][1] > x) --j;  // поиск элемента для переноса в младшую часть
+	          if(i <= j){           
+	              // обмен элементов местами:
+	              int[] temp = A[i];
+	              A[i] = A[j];
+	              A[j] = temp;
+	              // переход к следующим элементам:
+	              i++; j--;
+	          }
+	      } while(i < j);
+	      if(low < j) qSort(A, low, j);
+	      if(i < high) qSort(A, i, high);
+	  }
 }
